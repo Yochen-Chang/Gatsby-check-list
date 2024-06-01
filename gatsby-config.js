c087eba0ce6b01c1cd96a1c2eaef8c88ce1a4987
Yochen-Chang <hi.changyc@gmail.com>
2024-06-01 15:20:44 +0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `check-list`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
